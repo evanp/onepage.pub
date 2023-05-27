724f989da77b13c421184199f29d7a5545aed321
Evan Prodromou <evan@openearth.org>
2023-05-26 23:15:14 -0400

    feat(service): first version with a service endpoint
    Signed-off-by: Evan Prodromou <evan@openearth.org>

---
+++ index.mjs
@@ -0,0 +1,81 @@
+import express from 'express'
+import sqlite3 from 'sqlite3';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import fs from 'fs';
+import https from 'https';
+import wrap from 'express-async-handler';
+
+const DATABASE = process.env.OPP_DATABASE || ':memory:'
+const HOSTNAME = process.env.OPP_HOSTNAME || 'localhost'
+const PORT = process.env.OPP_PORT || 3000
+const KEY = process.env.OPP_KEY || 'server.key'
+const CERT = process.env.OPP_CERT || 'server.crt'
+
+// Initialize Express
+const app = express();
+
+// Initialize SQLite
+const db = new sqlite3.Database(DATABASE);
+db.run('CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) PRIMARY KEY, passwordHash VARCHAR(255), objectId VARCHAR(255))');
+db.run('CREATE TABLE IF NOT EXISTS object (id VARCHAR(255) PRIMARY KEY, data TEXT)');
+db.run('CREATE TABLE IF NOT EXISTS member (objectId VARCHAR(255), collectionId VARCHAR(255), FOREIGN KEY(objectId) REFERENCES object(id), FOREIGN KEY(collectionId) REFERENCES collection(id))');
+
+// Initialize Passport
+app.use(passport.initialize());
+// app.use(passport.session());
+
+// Local Strategy for login/logout
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+
+  }
+));
+
+app.get('/', wrap(async(req, res) => {
+  const url = req.originalUrl;
+  res.set('Content-Type', 'application/activity+json')
+  res.json({
+    '@context': 'https://w3c.org/ns/activitystreams',
+    'id': url,
+    'name': process.OPP_NAME || 'One Page Pub',
+    'type': 'Service'
+  })
+}))
+
+// Routes for ActivityPub
+app.get('/:username', wrap(async (req, res) => {
+  // TODO: handle actor
+}));
+
+app.get('/:username/inbox', wrap(async (req, res) => {
+  // TODO: handle inbox
+}));
+
+app.post('/:username/inbox', wrap(async (req, res) => {
+  // TODO: handle inbox
+}));
+
+// and so on...
+
+// Routes for authentication
+app.post('/register', wrap(async (req, res) => {
+  // TODO: handle registration
+}));
+
+app.post('/login', passport.authenticate('local'), wrap(async (req, res) => {
+  // TODO: handle login
+}));
+
+app.post('/logout', wrap(async (req, res) => {
+  // TODO: handle logout
+}));
+
+// Start server with SSL
+https.createServer({
+  key: fs.readFileSync(KEY),
+  cert: fs.readFileSync(CERT)
+}, app)
+.listen(PORT, HOSTNAME, () => {
+  console.log(`Listening on ${HOSTNAME}:${PORT}`)
+})
---
+++ index.test.mjs
@@ -0,0 +1,26 @@
+import {describe, before, after, it} from 'node:test';
+import {exec} from 'node:child_process';
+import https from 'node:https';
+import assert from 'node:assert';
+
+describe("The root object", () => {
+    let child = null
+    before(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Starting server')
+            child = exec('node index.mjs')
+            child.on('error', reject)
+            child.stdout.on('data', resolve)
+        })
+    })
+    after(() => {
+        console.log('ending server')
+        child.kill()
+        console.log('server ended')
+    })
+    it("can get the root object", async () => {
+        const res = await fetch('https://localhost:3000/')
+        const obj = await res.json()
+        assert.strictEqual(obj.type, 'Service')
+    })
+})
