1131f71a8fcc427ce01bc77f5fcca41ee0b22457
Ray <raypeaslee@gmail.com>
2023-12-23 14:23:55 -0500

    add express-rate-limit
    security issue

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import { rateLimit } from 'express-rate-limit'
 import sqlite3 from 'sqlite3'
 import passport from 'passport'
 import LocalStrategy from 'passport-local'
@@ -2318,6 +2319,16 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 // CSRF token middleware
 const csrf = wrap(async (req, res, next) => {
