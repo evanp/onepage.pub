5ee4f288579864ece71d3ff722482e68cd1c52b4
Ray <raypeaslee@gmail.com>
2023-12-31 12:30:02 -0500

    Update rate-limit.test.mjs
    add comments

--- rate-limit.test.mjs
+++ rate-limit.test.mjs
@@ -7,126 +7,125 @@ import 'dotenv/config'
 const MAIN_PORT = process.env.OPP_PORT
 console.log(`\nRate Limit: ${process.env.OPP_RATE_LIMIT}\n\nstarting rate limit tests\n`)
 
-describe('request rate limits', () => {
+/**
+ * Integration tests for rate limiting middleware.
+ *
+ * Sets up an HTTPS server and makes requests to test:
+ * - Requests per IP are limited
+ * - Requests below limit are allowed
+ * - Standard rate limit headers are used
+ */
+describe("request rate limits", () => {
+  const options = {
+    hostname: "localhost",
+    port: MAIN_PORT,
+    path: "/",
+    method: "GET",
+  };
 
-    const options = {
-        hostname: 'localhost',
-        port: MAIN_PORT,
-        path: '/',
-        method: 'GET'  
-    };
-  
-  beforeEach( () => {
-    
+  beforeEach(() => {
     const options = {
       key: readFileSync(process.env.OPP_KEY),
-      cert: readFileSync(process.env.OPP_CERT)
+      cert: readFileSync(process.env.OPP_CERT),
     };
-    
+
     try {
-      https.createServer(options, (req, res) => {
-        try {
-          res.writeHead(200);
-          res.end('hello world\n hello');
-        } catch (err) {
-          console.error(err);
-          res.statusCode = 500;
-          res.end('Internal Server Error'); 
-        }
-      }).listen(process.env.OPP_PORT);
+      https
+        .createServer(options, (req, res) => {
+          try {
+            res.writeHead(200);
+            res.end("hello world\n hello");
+          } catch (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Internal Server Error");
+          }
+        })
+        .listen(process.env.OPP_PORT);
     } catch (err) {
-      console.error('Error starting HTTPS server:', err);
+      console.error("Error starting HTTPS server:", err);
       process.exit(1);
     }
-  })
+  });
 
   afterEach(() => {
-    https.close
-    })
-
-    it('should limit requests per IP', async () => {
-      let nextCallCount = 0;
-       
-      // Call limiter middleware multiple times from same IP
-      for(let i = 0; i < +process.env.OPP_RATE_LIMIT + 50; i++) {
-        
-        const req = https.request(options, (res) => {
-
-          if (res.statusCode === 200) {
-              // success
-              nextCallCount++;
-            } else {
-              // handle error
-            }
-                     
-        });
-          req.on('error', (e) => {
-            if (e.message != 'self-signed certificate') {
-              console.error(`problem with request: ${e.message}`);
-            }
-          });
-                    
-          req.end();
-      }
-      return nextCallCount;
-      
-      // Assert request was blocked after exceeding limit
-      assert.strictEqual(nextCallCount, 100);
-    });
-      
-    it('should allow requests below limit', async () => {
-      let nextCallCount = 0;
-        
-      // Call limiter middleware below limit
-      for(let i = 0; i < +process.env.OPP_RATE_LIMIT - 50; i++) {
-        const req = https.request(options, (res) => {
-
-          if (res.statusCode === 200) {
-              // success
-              nextCallCount++;
-            } else {
-              // handle error
-            }
-                     
-        });
-          req.on('error', (e) => {
-            if (e.message != 'self-signed certificate') {
-              console.error(`problem with request: ${e.message}`);
-            }
-          });
-           
-          req.end();
-      }
-      return nextCallCount;
-  
-      // Assert all 50 requests were processed 
-      assert.strictEqual(nextCallCount, 50);
-    });
-    
-    it('uses standard headers', async () => {
-
-      const req = https.request(options, (res) => {
-
-        if (res.statusCode === 200) {
-            // success
-            assert(res.headers['RateLimit-Limit']);
-            assert(res.headers['RateLimit-Remaining']); 
-            assert(res.headers['RateLimit-Reset']);
-    
-            assert(!res.headers['X-RateLimit-Limit']);
-            assert(!res.headers['X-RateLimit-Remaining']);
-            assert(!res.headers['X-RateLimit-Reset']);
-          }
-               
-      });
-        req.on('error', (e) => {
-          if (e.message != 'self-signed certificate') {
-            console.error(`problem with request: ${e.message}`);
-          }
-        });
-         
-        req.end();
-    });
-  
+    https.close;
   });
+
+  it("should limit requests per IP", async () => {
+    let nextCallCount = 0;
+
+    // Call limiter middleware multiple times from same IP
+    for (let i = 0; i < +process.env.OPP_RATE_LIMIT + 50; i++) {
+      const req = https.request(options, (res) => {
+        if (res.statusCode === 200) {
+          // success
+          nextCallCount++;
+        } else {
+          // handle error
+        }
+      });
+      req.on("error", (e) => {
+        if (e.message != "self-signed certificate") {
+          console.error(`problem with request: ${e.message}`);
+        }
+      });
+
+      req.end();
+    }
+    return nextCallCount;
+
+    // Assert request was blocked after exceeding limit
+    assert.strictEqual(nextCallCount, 100);
+  });
+
+  it("should allow requests below limit", async () => {
+    let nextCallCount = 0;
+
+    // Call limiter middleware below limit
+    for (let i = 0; i < +process.env.OPP_RATE_LIMIT - 50; i++) {
+      const req = https.request(options, (res) => {
+        if (res.statusCode === 200) {
+          // success
+          nextCallCount++;
+        } else {
+          // handle error
+        }
+      });
+      req.on("error", (e) => {
+        if (e.message != "self-signed certificate") {
+          console.error(`problem with request: ${e.message}`);
+        }
+      });
+
+      req.end();
+    }
+    return nextCallCount;
+
+    // Assert all 50 requests were processed
+    assert.strictEqual(nextCallCount, 50);
+  });
+
+  it("uses standard headers", async () => {
+    const req = https.request(options, (res) => {
+      if (res.statusCode === 200) {
+        // success
+        assert(res.headers["RateLimit-Limit"]);
+        assert(res.headers["RateLimit-Remaining"]);
+        assert(res.headers["RateLimit-Reset"]);
+
+        assert(!res.headers["X-RateLimit-Limit"]);
+        assert(!res.headers["X-RateLimit-Remaining"]);
+        assert(!res.headers["X-RateLimit-Reset"]);
+      }
+    });
+    req.on("error", (e) => {
+      if (e.message != "self-signed certificate") {
+        console.error(`problem with request: ${e.message}`);
+      }
+    });
+
+    req.end();
+  });
+});
   
